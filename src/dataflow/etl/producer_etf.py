ceda353a437b6b4adff579c67038db59a253c416
pei yu ji <abc0970870@gmail.com>
2025-08-09 23:38:39 +0800

    commit

--- src/dataflow/etl/producer_etf.py
+++ src/dataflow/etl/producer_etf.py
@@ -9,7 +9,7 @@ def create_producer_task_tw() -> DockerOperator:
     return DockerOperator(
         # 設定這個 task 在 DAG 中的名稱（唯一識別碼）
         task_id="producer_main_tw",
-        image="peiyuji/tibame_crawler:0.0.10",
+        image="peiyuji/web_crawler:0.0.10",
         command="pipenv run python crawler/producer_main_tw.py",
         # 每次執行前都強制重新拉取最新的 image（確保使用最新版本）
         force_pull=True,
@@ -18,7 +18,7 @@ def create_producer_task_tw() -> DockerOperator:
         # ✅ 指定容器要使用的 Docker network 名稱
         # 注意：這要是 Docker Engine 中已存在的 network 名稱
         network_mode="etf_lib_network",
-        queue = "twse",
+        queue = "crawler_tw",
     )
 
 # 建立一個 DockerOperator 任務的函式，回傳一個 Airflow 的任務實例
@@ -26,7 +26,7 @@ def create_producer_task_us() -> DockerOperator:
     return DockerOperator(
         # 設定這個 task 在 DAG 中的名稱（唯一識別碼）
         task_id="producer_main_us",
-        image="peiyuji/tibame_crawler:0.0.10",
+        image="peiyuji/web_crawler:0.0.10",
         command="pipenv run python crawler/producer_main_us.py",
         # 每次執行前都強制重新拉取最新的 image（確保使用最新版本）
         force_pull=True,
@@ -35,5 +35,5 @@ def create_producer_task_us() -> DockerOperator:
         # ✅ 指定容器要使用的 Docker network 名稱
         # 注意：這要是 Docker Engine 中已存在的 network 名稱
         network_mode="etf_lib_network",
-        queue = "tpex",
+        queue = "crawler_us",
     )
