705b266f950075360fea4c3e7de31d7d8b6de962
pei yu ji <abc0970870@gmail.com>
2025-08-09 23:22:16 +0800

    commit

--- src/dataflow/config.py
+++ src/dataflow/config.py
@@ -3,10 +3,10 @@ import os
 WORKER_ACCOUNT = os.environ.get("WORKER_ACCOUNT", "worker")
 WORKER_PASSWORD = os.environ.get("WORKER_PASSWORD", "worker")
 
-RABBITMQ_HOST = os.environ.get("RABBITMQ_HOST", "35.209.250.210")
+RABBITMQ_HOST = os.environ.get("RABBITMQ_HOST", "35.208.165.47")
 RABBITMQ_PORT = int(os.environ.get("RABBITMQ_PORT", 5672))
 
-MYSQL_HOST = os.environ.get("MYSQL_HOST", "335.209.250.210")
+MYSQL_HOST = os.environ.get("MYSQL_HOST", "35.208.165.47")
 MYSQL_PORT = int(os.environ.get("MYSQL_PORT", 3306))
 MYSQL_ACCOUNT = os.environ.get("MYSQL_ACCOUNT", "root")
 MYSQL_PASSWORD = os.environ.get("MYSQL_PASSWORD", "ppWgnb_mfGe2m_")
--- src/dataflow/etl/producer_etf.py
+++ src/dataflow/etl/producer_etf.py
@@ -9,7 +9,7 @@ def create_producer_task_tw() -> DockerOperator:
     return DockerOperator(
         # 設定這個 task 在 DAG 中的名稱（唯一識別碼）
         task_id="producer_main_tw",
-        image="peiyuji/tibame_crawler:0.0.7",
+        image="peiyuji/tibame_crawler:0.0.10",
         command="pipenv run python crawler/producer_main_tw.py",
         # 每次執行前都強制重新拉取最新的 image（確保使用最新版本）
         force_pull=True,
@@ -26,7 +26,7 @@ def create_producer_task_us() -> DockerOperator:
     return DockerOperator(
         # 設定這個 task 在 DAG 中的名稱（唯一識別碼）
         task_id="producer_main_us",
-        image="peiyuji/tibame_crawler:0.0.7",
+        image="peiyuji/tibame_crawler:0.0.10",
         command="pipenv run python crawler/producer_main_us.py",
         # 每次執行前都強制重新拉取最新的 image（確保使用最新版本）
         force_pull=True,
